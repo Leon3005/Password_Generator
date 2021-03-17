1ad96f1e905c21af0d8eb0298200726c532e1e96
Leon <leonwheeler08@gmail.com>
2021-03-17 19:18:43 +0000

    Moved prompts into generatePassword function, created an if statement to check if user enters anything other than a number for the length.

--- Develop/script.js
+++ Develop/script.js
@@ -1,37 +1,100 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//Prompt for password length
-const passwordLengthPrompt = prompt("What is the required length of your password");
-
-//Prompt for lowercase
-const passwordLCasePrompt = confirm("Would you like to use lowercase characters?");
-
-//Prompt for uppercase
-const passwordUCasePrompt = confirm("Would you like to use uppercase characters?");
-
-//Prompt for numbers
-const passwordNumbersPrompt = confirm("Would you like to use numbers?");
-
-//Prompt for special characters
-const passwordSpecialPrompt = confirm("Would you like to use special characters?");
-
 //Characters for the generated password
-const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+const upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const specialCharacters = ["!", "?", "*", ".", ",", "$"];
 
 // Declare a new function on line 3
 function generatePassword() {
-	// Create a variable to store our generated password
-	let password = ''
+  // Create a variable to store our generated password
+  let password = "";
 
-	// Logic would go here
-	...
+  // Logic would go here
+  //Prompt for password length
+  let passwordLengthPrompt = prompt(
+    "What is the required length of your password. Must be between 8-128 characters."
+  );
+  // if statement to make sure user is inputting a number
+  if (isNaN(passwordLengthPrompt)) {
+    passwordLengthPrompt = prompt("Please enter a valid number.");
+  }
+  //Prompt for lowercase
+  const passwordLCasePrompt = confirm(
+    "Would you like to use lowercase characters?"
+  );
 
-	// Return our created password
-	return password;
+  //Prompt for uppercase
+  const passwordUCasePrompt = confirm(
+    "Would you like to use uppercase characters?"
+  );
+
+  //Prompt for numbers
+  const passwordNumbersPrompt = confirm("Would you like to use numbers?");
+
+  //Prompt for special characters
+  const passwordSpecialPrompt = confirm(
+    "Would you like to use special characters?"
+  );
+
+  // Return our created password
+  return password;
 }
 
 // Write password to the #password input
@@ -40,7 +103,6 @@ function writePassword() {
   let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
